543b22be92682eac1d832d2bf6231d33a0e7fec5
JohnDanielKim <danieljohnkim@ucla.edu>
2020-06-07 23:38:44 -0700

    deleting all previous functions; was thinking to leave them as comments to show what was previously there, but they make the doc harder to read and unnecessarily long renaming of file according to assignment description page on class website

--- hw1-versioncontrol/pic10b_vector.hpp
+++ hw1-versioncontrol/pic10b_vector.hpp
@@ -117,51 +117,31 @@ namespace Pic10b {
 	}
 	
 	/** *********************** OTHER MEMBERS *********************** **/
-//	bool vector::empty() const {
-//		return the_size == 0;
-//	}
 	template <typename T>
 	bool vector<T>::empty () const {
 		return the_size == 0;
 	}
 	
-//	size_t vector::size() const {
-//		return the_size;
-//	}
 	template <typename T>
 	size_t vector<T>::size () const {
 		return the_size;
 	}
 	
-//	size_t vector::capacity() const {
-//		return the_capacity;
-//	}
 	template <typename T>
 	size_t vector<T>::capacity () const {
 		return the_capacity;
 	}
 	
-//	double vector::front() const {
-//		return *the_data;
-//	}
 	template <typename T>
 	double vector<T>::front () const {
 		return *the_data;
 	}
 	
-//	double vector::back() const {
-//		return *(the_data + the_size - 1);
-//	}
 	template <typename T>
 	double vector<T>::back () const {
 		return *(the_data + the_size - 1);
 	}
 	
-//	double vector::at( size_t index ) const {
-//		if ( index < the_size )
-//			return the_data[index];
-//		return the_data[0];
-//	}
 	template <typename T>
 	double vector<T>::at( size_t index ) const {
 		if ( index < the_size ) {
@@ -170,28 +150,16 @@ namespace Pic10b {
 		return the_data[0];
 	}
 	
-//	double& vector::operator[]( size_t index ){
-//		return the_data[index];
-//	}
 	template <typename T>
 	double& vector<T>::operator [] (size_t index) {
 		return the_data[index];
 	}
 	
-//	double vector::operator[]( size_t index ) const {
-//		return the_data[index];
-//	}
 	template <typename T>
 	double vector<T>::operator [] (size_t index) const {
 		return the_data[index];
 	}
 	
-//	void vector::dump_data_to( std::ostream& out ) const {
-//		out << "Vector (dump): ";
-//		for ( size_t i = 0 ; i < the_capacity ; ++i )
-//			out << the_data[i] << ' ';
-//		out << '\n';
-//	}
 	template <typename T>
 	void vector<T>::dump_data_to (std::ostream& out) const {
 		out << "Vector (dump): ";
@@ -200,20 +168,11 @@ namespace Pic10b {
 		out << '\n';
 	}
 	
-//	void vector::dump_data() const {
-//		dump_data_to( std::cout );
-//	}
 	template <typename T>
 	void vector<T>::dump_data () const {
 		dump_data_to( std::cout );
 	}
 	
-//	void vector::push_back( double new_value ){
-//		if ( the_size == the_capacity )
-//			reserve( the_capacity + 1 );     // `the_data` is reassigned
-//
-//		the_data[the_size++] = new_value;
-//	}
 	template <typename T>
 	void vector<T>::push_back (double new_value) {
 		if (the_size == the_capacity) {
@@ -223,11 +182,6 @@ namespace Pic10b {
 
 	}
 	
-	// This implementation does not shrink the vector (ever)
-//	void vector::pop_back(){
-//		if ( the_size > 0 )
-//			--the_size;
-//	}
 	template <typename T>
 	void vector<T>::pop_back () {
 		if (the_size > 0) {
@@ -235,22 +189,6 @@ namespace Pic10b {
 		}
 	}
 	
-//	void vector::reserve( size_t new_capacity ){
-//		if ( new_capacity > the_capacity ) {
-//			if ( new_capacity <= 2 * the_capacity )
-//				new_capacity = 2 * the_capacity;
-//
-//			double* old_location = the_data;
-//
-//			the_data = new double[new_capacity];
-//			the_capacity = new_capacity;
-//
-//			for ( size_t i = 0 ; i < the_size ; ++i )
-//				the_data[i] = old_location[i];
-//
-//			delete old_location;
-//		}
-//	}
 	template <typename T>
 	void vector<T>::reserve (size_t new_capacity) {
 		if (new_capacity > the_capacity) {
@@ -335,11 +273,6 @@ namespace Pic10b {
 
 
 /** ************************ OTHER FUNCTIONS ************************ **/
-//std::ostream& operator<<( std::ostream& out, const Pic10b::vector& v ){
-//	for ( size_t i = 0 ; i < v.size() ; ++i )
-//		out << v[i] << ' ';
-//	return out;
-//}
 template <typename T>
 std::ostream& operator<< (std::ostream& out, const Pic10b::vector<T>& v) {
 	for ( size_t i = 0 ; i < v.size() ; ++i )
